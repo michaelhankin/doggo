4053f418a6d54c0540e76a922b609dae6f6e7b09
Michael Hankin <mjhankin1@gmail.com>
2025-05-01 10:41:35 -0400

    Refactor image loader

--- src/doggo/data/__init__.py
+++ src/doggo/data/__init__.py
@@ -1,41 +1,38 @@
 import os
 import re
-from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 from dataclasses import dataclass
 
 import numpy as np
 from numpy.typing import NDArray
 from PIL import Image
-from PIL.ImageFile import ImageFile
 
 
 @dataclass
-class LabeledImagePath:
-    label: int
-    img_path: str
+class TrainTestData:
+    Xtr: NDArray[np.float32]
+    ytr: NDArray[np.int32]
+    Xte: NDArray[np.float32]
+    yte: NDArray[np.int32]
 
 
 @dataclass
-class LabeledImage:
+class BreedInfo:
+    breed: str
+    imgs_path: str
     label: int
-    img_file: ImageFile
-
-
-@dataclass
-class LabeledImageVector:
-    label: int
-    img_vec: NDArray[np.float32]
 
 
 class DogBreedImageLoader:
-    BREED_DIR_RE = re.compile(r"^(n[0-9]{8}-)(?P<breed>[\w-]+)$")
+    BREED_DIR_RE = re.compile(r"^[0-9]+-(?P<breed>[\w-]+)$")
 
     IMG_DIR = "./data/images"
     IMG_MODE = "RGB"
 
-    IMG_LEN = 200
+    IMG_LEN = 50
     IMG_SIZE = (IMG_LEN, IMG_LEN)
-    IMG_VEC_LEN = 3 * IMG_LEN**2
+
+    # Take 10% as test data
+    TEST_SPLIT = 0.10
 
     def _get_breed_dirs(self) -> list[str]:
         breed_dirs = os.listdir(self.IMG_DIR)
@@ -47,82 +44,67 @@ class DogBreedImageLoader:
         sorted_breed_dirs = sorted(filtered_breed_dirs)
         return sorted_breed_dirs
 
-    def __init__(self):
-        self.breed_dirs = self._get_breed_dirs()
-        self.cpu_count = os.process_cpu_count()
-        if not self.cpu_count:
-            self.cpu_count = 8
-
-    def _load_labels(self) -> list[str]:
-        labels = []
-        for breed_dir in self.breed_dirs:
+    def _get_breed_names(self, breed_dirs: list[str]) -> list[str]:
+        breeds = []
+        for breed_dir in breed_dirs:
             parsed_breed = (
                 self.BREED_DIR_RE.match(breed_dir)["breed"].replace("_", " ").lower()
             )
-            labels.append(parsed_breed)
-        return labels
+            breeds.append(parsed_breed)
+        return breeds
 
-    def _get_N_imgs(self, breed_dir: str) -> int:
-        breed_path = os.path.join(self.IMG_DIR, breed_dir)
-        N_imgs = len(os.listdir(breed_path))
-        return N_imgs
+    def _get_breeds(self) -> list[BreedInfo]:
+        breed_dirs = self._get_breed_dirs()
+        breed_names = self._get_breed_names(breed_dirs)
+        breeds = []
+        for i, pair in enumerate(zip(breed_dirs, breed_names)):
+            breed_dir, breed_name = pair
+            imgs_path = os.path.join(self.IMG_DIR, breed_dir)
+            breed = BreedInfo(breed_name, imgs_path, i)
+            breeds.append(breed)
+        return breeds
 
-    def _load_img(self, labeled_img_path: LabeledImagePath) -> LabeledImage:
-        img_file = Image.open(labeled_img_path.img_path)
-        img_file.load()
-        labeled_img = LabeledImage(labeled_img_path.label, img_file)
-        return labeled_img
+    def __init__(self):
+        self.breeds = self._get_breeds()
 
-    def _get_labeled_img_paths_for_dir(
-        self, label: int, breed_dir: str
-    ) -> list[LabeledImagePath]:
-        output = []
-        breed_path = os.path.join(self.IMG_DIR, breed_dir)
-        for img_file in os.listdir(breed_path):
-            img_path = os.path.join(breed_path, img_file)
-            labeled_img_path = LabeledImagePath(label, img_path)
-            output.append(labeled_img_path)
-        return output
-
-    def _get_labeled_img_paths(self) -> list[LabeledImagePath]:
-        input_labels = list(range(len(self.breed_dirs)))
-        with ThreadPoolExecutor() as executor:
-            result = executor.map(
-                self._get_labeled_img_paths_for_dir, input_labels, self.breed_dirs
-            )
-        return list(el for batch in result for el in batch)
-
-    def _load_labeled_imgs(
-        self, labeled_img_paths: list[LabeledImagePath]
-    ) -> list[LabeledImage]:
-        with ThreadPoolExecutor() as executor:
-            result = executor.map(self._load_img, labeled_img_paths)
-        return list(result)
-
-    def _process_img(self, labeled_img: LabeledImage) -> LabeledImageVector:
-        img_file = labeled_img.img_file
-        img = img_file.resize(self.IMG_SIZE)
-        if img.mode != "RGB":
-            img = img.convert("RGB")
-        img_arr = np.array(img).astype("float32")
+    def _load_img_vec(self, img_path: str) -> NDArray[np.float32]:
+        img_file = Image.open(img_path).resize(self.IMG_SIZE)
+        if img_file.mode != self.IMG_MODE:
+            img_file = img_file.convert(self.IMG_MODE)
+        img_arr = np.array(img_file).astype("float32")
         img_vec = img_arr.flatten() / 255
-        labeled_img_vec = LabeledImageVector(labeled_img.label, img_vec)
-        return labeled_img_vec
+        return img_vec
 
-    def _process_imgs(
-        self, labeled_imgs: list[LabeledImage]
-    ) -> list[LabeledImageVector]:
-        batch_size = len(labeled_imgs) // self.cpu_count + 1
-        with ProcessPoolExecutor() as executor:
-            result = executor.map(self._process_img, labeled_imgs, chunksize=batch_size)
-        return list(result)
+    def _load_img_vecs(self, imgs_path: str) -> NDArray[np.float32]:
+        img_vecs = []
+        for img_file in os.listdir(imgs_path):
+            img_path = os.path.join(imgs_path, img_file)
+            img_vec = self._load_img_vec(img_path)
+            img_vecs.append([img_vec])
+        img_vecs = np.concatenate(img_vecs)
+        return img_vecs
 
-    def _load_X_y(self) -> tuple[NDArray, NDArray]:
-        labeled_img_paths = self._get_labeled_img_paths()
-        labeled_imgs = self._load_labeled_imgs(labeled_img_paths)
-        labeled_img_vecs = self._process_imgs(labeled_imgs)
-        X = np.concatenate(
-            [[labeled_img_vec.img_vec] for labeled_img_vec in labeled_img_vecs]
-        )
-        y = np.array([labeled_img_vec.label for labeled_img_vec in labeled_img_vecs])
-        return X, y
+    def _get_train_test_data_for_breed(self, breed: BreedInfo) -> TrainTestData:
+        X = self._load_img_vecs(breed.imgs_path)
+        N = X.shape[0]
+        y = np.full(N, breed.label, np.int32)
+        N_test = int(N * self.TEST_SPLIT)
+        Xte, Xtr = X[:N_test, :], X[N_test:, :]
+        yte, ytr = y[:N_test], y[N_test:]
+        data = TrainTestData(Xtr, ytr, Xte, yte)
+        return data
+
+    def _get_train_test_data(self) -> TrainTestData:
+        Xtr, ytr, Xte, yte = [], [], [], []
+        for breed in self.breeds:
+            data = self._get_train_test_data_for_breed(breed)
+            Xtr.append(data.Xtr)
+            ytr.append(data.ytr)
+            Xte.append(data.Xte)
+            yte.append(data.yte)
+        Xtr = np.concatenate(Xtr)
+        ytr = np.concatenate(ytr)
+        Xte = np.concatenate(Xte)
+        yte = np.concatenate(yte)
+        data = TrainTestData(Xtr, ytr, Xte, yte)
+        return data
